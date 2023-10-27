d03ebe80fdae30501e9d4a576c1d007991153cf9
vinessa <vinessa2323@gmail.com>
2023-10-28 07:55:10 +0800

    httpSSSSSSSSSSSSSSSSSSSSSSSSSSSSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
+
   resolve: {
     alias: {
       "@/components": path.resolve(__dirname, "./src/shadcn/components"),
       "@/lib": path.resolve(__dirname, "./src/shadcn/lib"),
     },
   },
-
-})
+  server: { https: true },
+});
